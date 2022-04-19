d67f69ef1880b1c763b185e7f3d855734cb3adee
anikaaurony <aurony111@gmail.com>
2022-04-19 21:07:20 +0600

    footer edited

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Login from './component/Login/Login';
 import Register from './component/Register/Register';
 import Checkout from './component/Checkout/Checkout';
 import RequiredAuth from './component/RequiredAuth/RequiredAuth';
+import Footer from './component/Shared/Footer/Footer';
 
 
 function App() {
@@ -18,26 +19,27 @@ function App() {
     <div >
       <Header></Header>
       <Routes>
-        <Route path="/" element={ <Home/>}/>
-        <Route path="/home" element={ <Home/>}/>
-        <Route path="/about" element={ <About/>} />
-        <Route path="/blog" element={ <Blog></Blog>} />
-        <Route path="/login" element={ <Login></Login>} />
-        <Route path="/register" element={ <Register></Register>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog></Blog>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/register" element={<Register></Register>} />
         <Route path="/checkout" element={
           <RequiredAuth>
             <Checkout></Checkout>
           </RequiredAuth>
         }></Route>
-        <Route path="*" element={ <NotFound></NotFound>} />
-        
+        <Route path="*" element={<NotFound></NotFound>} />
+
 
 
       </Routes>
-      
-      
-     
-      
+      <Footer></Footer>
+
+
+
+
     </div>
   );
 }
---
+++ src/component/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p style={{textAlign: 'center'}}><strong>Copywrite @ 2022</strong></p>
+        </div>
+    );
+};
+
+export default Footer;
