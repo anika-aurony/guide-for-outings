51263cbb3cfebd6b5403bebb93832748f8d11087
anikaaurony <aurony111@gmail.com>
2022-04-19 20:09:43 +0600

    about added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+
+            <h3 style={{textAlign: 'center'}} className='mt-5'>Anika Tasnim Aurony</h3>
+            <div className='p-5 mt-2'>
+                <p>My goal is to be a full stack web developer. I want to workhard to achieve maximum skills that need to be a worldclass web developer. My dream is to work in top ranking companies like google, facebook, amazon etc. I want to enhance my knowledge in IT sector to reach my goal.</p>
+                <p>To achieve my goal there is no other alternatives other than practices. I need to practice more and more. Need to give more time.</p>
+            </div>
         </div>
     );
 };
